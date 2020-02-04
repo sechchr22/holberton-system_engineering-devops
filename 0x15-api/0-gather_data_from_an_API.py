9319860f83eb95f95750c4efd20e0d73019373a1
sechchr22 <980@holbertonschool.com>
2020-02-04 21:02:44 +0000

    Get data from an API and show it with a format

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -11,8 +11,8 @@ if __name__ == "__main__":
     URL_name = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
     URL_todos = 'https://jsonplaceholder.typicode.com/todos'
 
-    r_name = requests.get(url = URL_name)
-    r_todos = requests.get(url = URL_todos)
+    r_name = requests.get(url=URL_name)
+    r_todos = requests.get(url=URL_todos)
 
     dict_name = r_name.json()
     dict_todos = r_todos.json()
@@ -23,15 +23,18 @@ if __name__ == "__main__":
     TASKS = ''
 
     for dictio in dict_todos:
-    
-        if dictio['completed'] is true:
-            NUMBER_OF_DONE_TASKS += 1
-    
-        TOTAL_NUMBER_OF_TASKS += 1
 
-        TASKS += '\t' + dictio['title'] + '\n'
+        if (dictio.get('userId') is int(user_id)):
 
-    LINE = '{} is done with tasks({}/{}):'.format(\
+            if (dictio.get('completed')):
+                NUMBER_OF_DONE_TASKS += 1
+
+            TOTAL_NUMBER_OF_TASKS += 1
+
+            TASKS += '\t' + dictio['title'] + '\n'
+
+    TASKS = TASKS.rstrip()
+    LINE = '{} is done with tasks({}/{}):'.format(
            EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS)
 
     print('{}\n{}'.format(LINE, TASKS))
